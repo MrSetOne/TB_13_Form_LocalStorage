6e156e079468ab6843952ac4d4543e0e2d9c80aa
MrSetOne <lara.sanchez.michael@gmail.com>
2022-04-27 22:05:45 +0200

    done

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
     - [X] Correo
     - [X] Mensaje
 - [X] Guardar en Local Storage los datos de contacto enviados de cada usuario
-- [ ] Mostrar los datos de los contactos guardados en el DOM
+- [X] Mostrar los datos de los contactos guardados en el DOM
 - [X] Usa JSON.parse() y JSON.stringify() para guardar muchos datos usando la misma clave
 ## 2. Avanzado - Local Storage
-- [ ] Crea un botón para borrar todos los contactos guardados en Local Storage
-- [ ] Crea un  botón para borrar un contacto en concreto de Local Storage.
+- [X] Crea un botón para borrar todos los contactos guardados en Local Storage
+- [X] Crea un  botón para borrar un contacto en concreto de Local Storage.
--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const btn = document.querySelector("#btn")
 const form = document.querySelector("#form")
 const show = document.querySelector(".main__container")
+const delater = document.querySelector("#formDelate")
 let db = []
 
 
@@ -9,21 +10,32 @@ let db = []
 if (localStorage.db != undefined) {
     db = toArray(localStorage.db)
 }
+if (localStorage.idCounter == undefined) {
+    localStorage.setItem("idCounter", 0)
+}
+
+delater.addEventListener("click", () => {
+    db = [];
+    localStorage.clear();
+    localStorage.setItem("idCounter", 0)
+    show.innerHTML = ""
+})
 
 form.addEventListener("submit", (x) => {
     x.preventDefault()
-    console.log("ha funcionado")
     let user = {};
+    localStorage.idCounter++;
     user.name = document.querySelector("#formName").value;
     user.mail = document.querySelector("#formMail").value;
     user.message = document.querySelector("#formText").value;
-    user.id = (db.length + 1)
-    console.log(user)
+    user.id = localStorage.idCounter
     db.push(user)
-    console.log(db)
     localStorage.db = toJson(db)
+    show.innerHTML = ""
     showMe(db)
-
+    document.querySelector("#formName").value = "";
+    document.querySelector("#formMail").value = "";
+    document.querySelector("#formText").value = "";
 })
 
 function toJson(update) {
@@ -36,34 +48,32 @@ function toArray(update) {
 
 function showMe(arr) {
     for (const item of arr) {
-        let nuevo = document.createElement("li")
-            // show.nuevo.innerHTML = `            
-            //                     <div class="item">
-            //                         <p>#${item.id}</p>
-            //                         <div class="item__content">
-            //                             <h2>${item.mail}</h2>
-            //                             <h3>${item.mail}</h3>
-            //                             <p>${item.message}</p>
-            //                         </div>
-            //                     </div>
-            //                      `
-
-
-
-
-
-
-
-
-        show.innerHTML = `            
-                            <div class="item">
-                                <p>#${item.id}</p>
-                                <div class="item__content">
-                                    <h2>${item.mail}</h2>
-                                    <h3>${item.mail}</h3>
-                                    <p>${item.message}</p>
+        show.innerHTML += `            
+                            <li class="item">
+                                <div>
+                                    <p>#${item.id}</p>
+                                    <div class="item__content">
+                                        <h2>${item.name}</h2>
+                                        <h3>${item.mail}</h3>
+                                    </div>
+                                    <div onclick="killer(${item.id})"><i class="fa-solid fa-trash-can"></i></div>
                                 </div>
-                            </div>
+                                <p>${item.message}</p>
+                            </li>
                             `
     }
 }
+
+function killer(toKill) {
+    for (const element of db) {
+        if (element.id == toKill) {
+            let dead = db.indexOf(element)
+            db.splice(dead, 1);
+            show.innerHTML = ""
+            showMe(db)
+            localStorage.db = toJson(db)
+        }
+    }
+}
+
+showMe(db)
